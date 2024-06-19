662e3b3f408a355954de8f301096d3d63f543a18
Carlos V <carlos.villarroel.68@gmail.com>
2024-06-19 00:34:13 -0400

    optimizacion y rutina en paralelo

--- run.py
+++ run.py
@@ -2,25 +2,27 @@ import pandas as pd
 import numpy as np
 import random 
 import matplotlib.pyplot as plt
+import multiprocessing
 from utils import *
 
-if __name__ == "__main__":
-    PATH: str = 'results/' # Carpeta donde se guardarán los resultados
-    
-    number_of_simulations = 2 #Número de simulaciones a realizar
-    iteraciones = 60   # numero de iteraciones por simulación
-    lambda_ML_vector = np.logspace(-3,1 , iteraciones)  # Vector de lambdas a probar, en escala logarítmica
-    presicion = 1e-11 # Presición para la minimización
-    
-    N  = 100 # Número de eslabones y partículas
-    l0 = 1 # Longitud de los eslabones
-    k  = 1  # Constante del resorte
-    phi  = np.pi/4 # angulo inicial
+PATH_RESULTS = 'results/'
+N = 64*32 # Número de eslabones y partículas
 
-    for simulation in range(number_of_simulations):
+def run_simulation(simulation):
+        lambda_ML_min = -6 # Mínimo valor de lambda en --ESCALA LOGARÍTMICA--
+        lambda_ML_max = 1  # Máximo valor de lambda en --ESCALA LOGARÍTMICA--
+        iteraciones = (lambda_ML_max - lambda_ML_min)*5 + 1 # Número de iteraciones
+        lambda_ML_vector = np.logspace(lambda_ML_min, lambda_ML_max, iteraciones) # Vector de lambdas
+        
+        presicion = 1e-10 # Presición para la minimización
+
+        l0 = 1 # Longitud de los eslabones
+        k  = 1  # Constante del resorte
+        phi  = np.pi/4 # angulo inicial
+        
         seed = simulation + 123
         x0, y0, phis0, thetas0, L0, L_max= make_spring(N, l0, phi, seed) # Crear resorte
-        save_spring(x0, y0, phis0, thetas0, N, simulation, 0, PATH)
+        save_spring(x0, y0, phis0, thetas0, N, simulation, 0, PATH_RESULTS) # Guardar estado inicial
         
         phis = phis0.copy() # Copiar phis para guardar el estado inicial
         step_size = 0.1  # Tamaño del paso de integración para minimización
@@ -45,14 +47,46 @@ if __name__ == "__main__":
                 if f_aux_2 < f_aux_1:
                     alpha_CG = f_aux_2/f_aux_1
                 else:
-                    alpha_CG = f_aux_1/f_aux_2
-                
+                    #alpha_CG = f_aux_1/f_aux_2
+                    alpha_CG = 0.0
+                    
                 f_aux_1 = f_aux_2    
                 
-            L = calculate_total_extention(phis, l0)
-            data.append({'L': L, 'L_max': L_max, 'lambda': lambda_ML})
-            print('Step:', itr+1, 'Largo actual:', L, 'Largo máximo:', L_max, 'lambda:', lambda_ML)
-        save_evolution(data, N, simulation, PATH)
+            L = calculate_total_extention(phis, l0) # Calcular la extensión total del resorte en el paso actual
+            
+            # Guardar información de la simulación en la lista para guardarla en un archivo
+            data.append({'L': L, 
+                         'L_max': L_max, 
+                         'L_0': L0, 
+                         'lambda': lambda_ML}) 
+            
+            # Imprimir en consola el paso actual --NO USAR EN PARALELO-- 
+            #print('Step:', itr+1, 'Largo actual:', L, 'Largo máximo:', L_max, 'lambda:', lambda_ML) 
+        
+        save_evolution(data, N, simulation, PATH_RESULTS) # Guardar evolución de la simulación
+
+
+
+RUN_IN_PARALLEL = True # Correr simulaciones en paralelo
+NUM_CORES = 16 # Número de núcleos a utilizar
+
+if __name__ == "__main__":        
+    number_of_simulations = 16 #Número de simulaciones a realizar   
+    
+    if RUN_IN_PARALLEL:
+        pool = multiprocessing.Pool(processes=NUM_CORES)
+        pool.map(run_simulation, range(number_of_simulations))
+        pool.close()
+        pool.join()
+    else:
+        for simulation in range(number_of_simulations):
+            run_simulation(simulation)
+
+        
+        
+    
+        
+        
         
 
 
